925b8fd9b55e37b40d607d07b49c44f142457bc9
ali-hesham <ali.hesham@outlook.com>
2021-10-03 23:20:28 +0200

    initialize producer and populate topic test_topic with data

---
+++ producer.py
@@ -0,0 +1,14 @@
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
+
+# initialize kafka producer
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=lambda x:
+                         dumps(x).encode('utf-8'))
+
+# generate numbers from 0 to 999 and send it to broker in a topic test_topic
+for e in range(1000):
+    data = {'number': e}
+    producer.send('test_topic', value=data)
+    sleep(5)  # sleep for 5 seconds to make sure message delivered
